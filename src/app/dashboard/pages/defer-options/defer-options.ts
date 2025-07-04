72e32538e1fc13be4f142d91e7ec613d491e9a67
marco-soria <marco.soriaa7@gmail.com>
2025-07-04 09:38:14 -0500

    defer options

--- src/app/dashboard/pages/defer-options/defer-options.ts
+++ src/app/dashboard/pages/defer-options/defer-options.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeavyLoadersFast } from '../../../shared/heavy-loaders/heavy-loaders-fast';
+import { Title } from '../../../shared/title/title';
 
 @Component({
   selector: 'app-defer-options',
-  imports: [],
+  imports: [HeavyLoadersFast, Title],
   templateUrl: './defer-options.html',
 })
 export default class DeferOptions {}
