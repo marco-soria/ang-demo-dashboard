a9012674a82f1d429d6982bbab775db2dacf9b44
marco-soria <marco.soriaa7@gmail.com>
2025-07-04 09:11:11 -0500

    corrected control flow and added change detection

--- src/app/dashboard/pages/change-detection/change-detection.ts
+++ src/app/dashboard/pages/change-detection/change-detection.ts
@@ -1,8 +1,48 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
+import { Title } from '@shared/title/title';
 
 @Component({
   selector: 'app-change-detection',
-  imports: [],
-  templateUrl: './change-detection.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [Title, JsonPipe],
+  template: `
+    <app-title [title]="currentFramework()" />
+
+    <pre> {{ frameworkAsSignal() | json }} </pre>
+    <pre> {{ frameworkAsProperty | json }} </pre>
+  `,
 })
-export default class ChangeDetection {}
+export default class ChangeDetection {
+  public currentFramework = computed(
+    () => `Change detection - ${this.frameworkAsSignal().name}`
+  );
+
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // this.frameworkAsProperty.name = 'React';
+      this.frameworkAsSignal.update((value) => {
+        value.name = 'React';
+
+        return { ...value };
+      });
+
+      console.log('Hecho');
+    }, 3000);
+  }
+}
