de632ff349432ba57f81ab3a57bdbe82e36c95db
marco-soria <marco.soriaa7@gmail.com>
2025-07-04 08:41:53 -0500

    sidemenu

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Sidemenu } from '../shared/sidemenu/sidemenu';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidemenu],
   templateUrl: './dashboard.html',
 })
 export default class Dashboard {}
--- src/app/shared/sidemenu/sidemenu.ts
+++ src/app/shared/sidemenu/sidemenu.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-sidemenu',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidemenu.html',
-  styleUrl: './sidemenu.css'
+  styleUrl: './sidemenu.css',
 })
 export class Sidemenu {
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
 
+  constructor() {
+    // const dashboardRoutes = routes
+    //   .map( route => route.children ?? [] )
+    //   .flat()
+    //   .filter( route => route && route.path )
+    //   .filter( route => !route.path?.includes(':') )
+    // console.log(dashboardRoutes);
+  }
 }
